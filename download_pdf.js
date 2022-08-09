cfa111145c8adcf77fe4f6f993bc43bb27dbb81e
Rizky Luthfianto <mrluthfianto@gmail.com>
2022-08-09 16:36:01 +0700

    set PDF page ratio

--- download_pdf.js
+++ download_pdf.js
@@ -1,18 +1,23 @@
 let jspdf = document.createElement("script");
 jspdf.onload = function() {
-    let pdf = new jsPDF();
+    const ratio = 3.8;
+    let pdf;
     let elements = document.getElementsByTagName("img");
     for (let i in elements) {
         let img = elements[i];
+        let width = img.width;
+        let height = img.height;
         if (!/^blob:/.test(img.src)) {
             continue;
         }
         let canvasElement = document.createElement('canvas');
         let con = canvasElement.getContext("2d");
-        canvasElement.width = img.width;
-        canvasElement.height = img.height;
-        con.drawImage(img, 0, 0, img.width, img.height);
+        canvasElement.width = width;
+        canvasElement.height = height;
+        con.drawImage(img, 0, 0, width, height);
         let imgData = canvasElement.toDataURL("image/jpeg", 1.0);
+        if (!pdf)
+            pdf = new jsPDF('l', 'mm', [width / ratio, height / ratio]);;
         pdf.addImage(imgData, 'JPEG', 0, 0);
         pdf.addPage();
     }
